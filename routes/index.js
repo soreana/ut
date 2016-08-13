5e52e1028e71d31b82447bff73f03a887c638fa5
soreana <esterlinkof@gmail.com>
2016-08-13 13:48:35 +0430

    add post route for create new tag.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Tag = mongoose.model('Tag');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/tag', function(req, res, next) {
+  var tag = new Tag(req.body);
+
+  tag.save(function(err, tag){
+    if(err){ return next(err); }
+
+    res.json(tag);
+  });
+});
+
+router.get('/tags',function (req,res,next){
+  Tag.find(function (err,tags) {
+    if(err) {return next(err)}
+    res.json(tags);
+  });
 });
 
 module.exports = router;
